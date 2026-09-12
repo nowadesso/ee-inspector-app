ad6f73c5b96af636ff2b101e2422dca6395bd14f
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-12 23:31:44 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(__file__), 'modules'))
-
 from kivymd.app import MDApp
 from kivymd.uix.screen import MDScreen
 from kivymd.uix.boxlayout import MDBoxLayout
