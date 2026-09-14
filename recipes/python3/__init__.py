dc8d9cd23547e594657039f2f01dfe7e9044168b
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-14 23:19:54 +0800

    Update __init__.py

--- recipes/python3/__init__.py
+++ recipes/python3/__init__.py
@@ -1,1 +1,11 @@
+from pythonforandroid.recipe import Recipe
 
+
+class Python3Recipe(Recipe):
+    version = "3.12.10"
+    url = "https://www.python.org/ftp/python/{version}/Python-{version}.tgz"
+    depends = ["hostpython3", "sqlite3", "openssl", "libffi"]
+    patches = ["patches/disable_grp.patch"]
+
+
+recipe = Python3Recipe()
