941e493bdbd6ead5dc8f43948db8443efd828246
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-15 13:26:42 +0800

    Update __init__.py

--- recipes/python3/__init__.py
+++ recipes/python3/__init__.py
@@ -1,11 +1,47 @@
-from pythonforandroid.recipe import Recipe
+from pythonforandroid.recipe import TargetPythonRecipe
 
 
-class Python3Recipe(Recipe):
+class Python3Recipe(TargetPythonRecipe):
     version = "3.12.10"
     url = "https://www.python.org/ftp/python/{version}/Python-{version}.tgz"
-    depends = ["hostpython3", "sqlite3", "openssl", "libffi"]
+
+    depends = [
+        "hostpython3",
+        "sqlite3",
+        "openssl",
+        "libffi",
+    ]
+
     patches = []
+
+    def include_root(self, arch_name):
+        from os.path import join
+        return join(
+            self.get_build_dir(arch_name),
+            "android-build",
+            "android-root",
+            "include",
+            "python3.12",
+        )
+
+    def link_root(self, arch_name):
+        from os.path import join
+        return join(
+            self.get_build_dir(arch_name),
+            "android-build",
+        )
+
+    @property
+    def link_version(self):
+        return "3.12"
+
+    def get_python_root(self, arch):
+        from os.path import join
+        return join(
+            self.get_build_dir(arch.arch),
+            "android-build",
+            "android-root",
+        )
 
 
 recipe = Python3Recipe()
