8717cb9f9a73fa19c08919fce0ba37d54ca6e8b0
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-15 13:13:49 +0800

    Update __init__.py

--- recipes/python3/__init__.py
+++ recipes/python3/__init__.py
@@ -5,7 +5,7 @@ class Python3Recipe(Recipe):
     version = "3.12.10"
     url = "https://www.python.org/ftp/python/{version}/Python-{version}.tgz"
     depends = ["hostpython3", "sqlite3", "openssl", "libffi"]
-    patches = ["patches/disable_grp.patch"]
+    patches = []
 
 
 recipe = Python3Recipe()
