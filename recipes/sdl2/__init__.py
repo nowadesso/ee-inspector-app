c1e96d6e27384d9f45754027f84ac208490758c8
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-21 22:57:38 +0800

    Update __init__.py

--- recipes/sdl2/__init__.py
+++ recipes/sdl2/__init__.py
@@ -1,1 +1,9 @@
+from pythonforandroid.recipes.sdl2 import SDL2Recipe as _Base
 
+
+class SDL2Recipe(_Base):
+    version = '2.28.5'
+    url = 'http://mirror.koddos.net/blfs/conglomeration/SDL/SDL2-{version}.tar.gz'
+
+
+recipe = SDL2Recipe()
