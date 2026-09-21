d9c92ea53c78b8ad1c22c0164b89736703c07882
nowadesso <140019442+nowadesso@users.noreply.github.com>
2026-09-22 04:06:33 +0800

    Update __init__.py

--- recipes/jpeg/__init__.py
+++ recipes/jpeg/__init__.py
@@ -1,1 +1,13 @@
+from pythonforandroid.recipes.jpeg import JPEGRecipe as _Base
 
+
+class JPEGRecipe(_Base):
+
+    def get_recipe_env(self, arch, with_flags_in_cc=True):
+        env = super().get_recipe_env(arch, with_flags_in_cc)
+        # Inject the CMake policy version fix for CMake 4.x
+        env['CMAKE_POLICY_VERSION_MINIMUM'] = '3.5'
+        return env
+
+
+recipe = JPEGRecipe()
